222e75dbb56421211f1fbb09a4c5ee89d1e0f640
robert rochlin <rrochlin@yahoo.com>
2025-04-03 22:39:50 -0700

    feat: :rocket: recolors viewer code blocks

---
+++ content.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const elements = document.querySelectorAll('.viewer code');
+    elements.forEach(element => {
+      element.style.backgroundColor = 'var(--gray-750)';
+    });
+  });
+  
